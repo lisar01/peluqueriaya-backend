83f3f4eb8e6805e65820041f079c8158779a475a
cassa10 <cassanojoseluis@gmail.com>
2020-04-11 19:45:44 -0300

    Agregando metodo para poder crear datos falsos, una vez inicie la aplicacion

--- src/main/kotlin/ar/edu/unq/peluqueriayabackend/PeluqueriayaBackendApplication.kt
+++ src/main/kotlin/ar/edu/unq/peluqueriayabackend/PeluqueriayaBackendApplication.kt
@@ -1,7 +1,10 @@
 package ar.edu.unq.peluqueriayabackend
 
+import ar.edu.unq.peluqueriayabackend.persistence.impl.repositories.PeluqueroRepository
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
@@ -17,6 +20,21 @@ class PeluqueriayaBackendApplication : WebMvcConfigurer{
 	override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
 		registry.addResourceHandler("swagger-ui.html")
 				.addResourceLocations("classpath:/META-INF/resources/")
+	}
+
+	@Bean
+	fun demo(peluqueroRepository: PeluqueroRepository): CommandLineRunner? {
+		return CommandLineRunner { args: Array<String?>? ->
+			System.out.println("Descomente el metodo: createFakeData(..) en la clase PeluqueriayaBackendApplication, para generar datos falsos")
+			System.out.println("|**!!ADVICE!!**| Una vez ejecutado el metodo createFakeData(..) vuelva a comentarlo o elimine los datos de la Base de Datos, para evitar errores")
+			this.createFakeData(peluqueroRepository)
+
+		}
+	}
+
+	fun createFakeData(peluqueroRepository: PeluqueroRepository) {
+		System.out.println("|**!!WARNING!!**| Metodo createFakeData(..) en clase PeluqueriayaBackendApplication no comentado esto puede generar errores si los datos ya existen")
+
 	}
 
 }
