e558023f7ad05f9c2d3e8ac0541da81e031e390d
Jose Cassano <39202199+cassa10@users.noreply.github.com>
2020-06-07 17:45:35 -0300

    Issue/visualizar datos cliente en turnos (#28)
    * Ahora cuando se obtienen los turnos, se les pasa la direccion de la ubicacion del cliente

---
+++ src/main/kotlin/ar/edu/unq/peluqueriayabackend/controller/dtos/TurnoConDireccionDTO.kt
@@ -0,0 +1,87 @@
+package ar.edu.unq.peluqueriayabackend.controller.dtos
+
+import ar.edu.unq.peluqueriayabackend.model.*
+import com.fasterxml.jackson.annotation.JsonIgnore
+import java.math.BigDecimal
+import java.time.LocalDateTime
+
+class TurnoConDireccionDTO(
+        @JsonIgnore
+        var peluquero: Peluquero,
+        @JsonIgnore
+        var cliente: Cliente,
+        var serviciosSolicitadosInfo: MutableList<ServicioInfo>,
+        var fechaInicio: LocalDateTime = LocalDateTime.now(),
+        var fechaCancelacion: LocalDateTime?,
+        var fechaPendiente: LocalDateTime?,
+        var fechaConfirmacion: LocalDateTime?,
+        var fechaFin: LocalDateTime?,
+        var estado: TurnoState = TurnoState.ESPERANDO,
+        var puntaje: Int = 0,
+        var corteMinInfo: BigDecimal,
+        var ubicacionDelTurno: Ubicacion,
+        var direccionDelTurno:String,
+        var id: Long? = null
+)
+{
+
+    fun getPeluqueroId():Long? = peluquero.id
+
+    fun getClienteId():Long? = cliente.id
+
+    fun getEstaPendiente():Boolean = estado.estaPendiente()
+
+    fun getEstaConfirmado():Boolean = estado.estaConfirmado()
+
+    fun getEstaFinalizado():Boolean = estado.estaFinalizado()
+
+    fun getEstaCancelado():Boolean = estado.estaCancelado()
+
+    fun getEstaEsperando():Boolean = estado.estaEsperando()
+
+    fun getClienteEmail():String = cliente.emailOpcional
+
+    fun getClienteFullName():String = cliente.getFullName()
+
+    class Builder(
+            var peluquero: Peluquero = Peluquero.Builder().build(),
+            var cliente: Cliente = Cliente.Builder().build(),
+            var serviciosSolicitadosInfo: MutableList<ServicioInfo> = mutableListOf(),
+            var fechaInicio: LocalDateTime = LocalDateTime.now(),
+            var fechaCancelacion: LocalDateTime? = null,
+            var fechaPendiente: LocalDateTime? = null,
+            var fechaConfirmacion: LocalDateTime? = null,
+            var fechaFin: LocalDateTime? = null,
+            var estado: TurnoState = TurnoState.ESPERANDO,
+            var puntaje: Int = 0,
+            var corteMinInfo: BigDecimal = BigDecimal.ZERO,
+            var ubicacionDelTurno: Ubicacion = Ubicacion("",""),
+            var direccionDelTurno:String = "",
+            var id: Long? = null
+    ){
+        fun build() : TurnoConDireccionDTO{
+            return TurnoConDireccionDTO(peluquero, cliente,serviciosSolicitadosInfo, fechaInicio,
+                fechaCancelacion, fechaPendiente, fechaConfirmacion, fechaFin, estado, puntaje,
+                    corteMinInfo, ubicacionDelTurno, direccionDelTurno, id)
+        }
+
+        fun withTurno(turno: Turno) = apply {
+            peluquero = turno.peluquero
+            cliente = turno.cliente
+            serviciosSolicitadosInfo = turno.serviciosSolicitadosInfo
+            fechaInicio = turno.fechaInicio
+            fechaCancelacion = turno.fechaCancelacion
+            fechaPendiente = turno.fechaPendiente
+            fechaConfirmacion = turno.fechaConfirmacion
+            fechaFin = turno.fechaFin
+            estado = turno.estado
+            puntaje = turno.puntaje
+            corteMinInfo = turno.corteMinInfo
+            ubicacionDelTurno = turno.ubicacionDelTurno
+            id = turno.id
+        }
+
+        fun withDireccionDelTurno(direccionDelTurno: String) = apply { this.direccionDelTurno = direccionDelTurno }
+
+    }
+}
