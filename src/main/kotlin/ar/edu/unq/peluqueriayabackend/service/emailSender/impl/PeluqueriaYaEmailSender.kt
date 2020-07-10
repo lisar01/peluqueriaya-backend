8166ff1184a2da575813fcd8a6f53148a913613b
Jose Cassano <39202199+cassa10@users.noreply.github.com>
2020-07-09 21:02:52 -0300

    Se mejora body de los emails. Update fake data. (#48)

--- src/main/kotlin/ar/edu/unq/peluqueriayabackend/service/emailSender/impl/PeluqueriaYaEmailSender.kt
+++ src/main/kotlin/ar/edu/unq/peluqueriayabackend/service/emailSender/impl/PeluqueriaYaEmailSender.kt
@@ -9,39 +9,88 @@ import org.springframework.stereotype.Service
 class PeluqueriaYaEmailSender(@Autowired val emailServiceAPI: EmailServiceAPI) {
 
     fun enviarMailDeConfirmacion(turno: Turno){
-        emailServiceAPI.sendEmail(turno.cliente.emailOpcional,
-                "El turno fue confirmado",
-                "Hola el turno ha sido confirmado! Aproximadamente, en los proximos 30 minutos el peluquero " +
-                        "llegará a la ubicacion especificada. Gracias por utilizar el servicio! Saludos :D!")
+        val bodyBuilder = StringBuilder()
+        createBody(bodyBuilder) {
+            bodyBuilder.append("El turno ha sido confirmado!! Aproximadamente, en los próximos minutos el peluquero")
+            bodyBuilder.appendln(" llegará a la ubicacion especificada.")
+            bodyBuilder.appendln("Los datos de contacto del peluquero se encuentran en su pestaña de turnos, ante cualquier duda.")
+        }
+
+        emailServiceAPI.sendEmail(turno.getClienteEmail(),
+                agregarNoReply("El turno fue confirmado"),
+                bodyBuilder.toString()
+        )
     }
 
     fun enviarMailAlClienteQueTerminoSuEspera(turno: Turno) {
-        emailServiceAPI.sendEmail(turno.cliente.emailOpcional,
-        "El turno ahora esta en Pendiente",
-        "Hola el turno está en pendiente, eso quiere decir que esta cada vez mas cerca de ser atendido!")
+        val bodyBuilder = StringBuilder()
+        createBody(bodyBuilder) {
+            bodyBuilder.appendln("El turno está en estado \"Pendiente\", eso quiere decir que esta cada vez mas cerca de ser atendido!")
+        }
+
+        emailServiceAPI.sendEmail(turno.getClienteEmail(),
+                agregarNoReply("El turno ahora esta en Pendiente"),
+                bodyBuilder.toString()
+        )
     }
 
     fun enviarMailAlClienteQueSeCanceloElTurno(turno: Turno) {
-        emailServiceAPI.sendEmail(turno.cliente.emailOpcional,
-        "El turno fue cancelado",
-        "El turno fue cancelado lamentablemente :C. " +
-                "Puede pedir otro turno a otro peluquero o volver a intentar con el mismo si este está conectado!")
+        val bodyBuilder = StringBuilder()
+        createBody(bodyBuilder) {
+            bodyBuilder.appendln("El turno con el peluquero \"${turno.getPeluqueroName()}\" fue cancelado lamentablemente :C.")
+            bodyBuilder.appendln("Puede pedir otro turno a otro peluquero o volver a intentar con el mismo si este está conectado!")
+        }
+        emailServiceAPI.sendEmail(turno.getClienteEmail(),
+                agregarNoReply("El turno fue cancelado"),
+                bodyBuilder.toString()
+        )
     }
 
     fun enviarMailAlClienteQueSuTurnoEstaEnEspera(turno:Turno) {
-        emailServiceAPI.sendEmail(turno.cliente.emailOpcional,
-        "El turno esta en la cola de espera del peluquero",
-        "Hola, su turno fue ingresado en la cola de espera del peluquero porque este posee completa su casilla de turnos. "+
-        "Es muy probable que su turno demore mucho, si quiere puede cancelar el turno en su perfil de turnos. Saludos! :D")
+        val bodyBuilder = StringBuilder()
+        createBody(bodyBuilder) {
+            bodyBuilder.appendln("Su turno fue ingresado en la cola de espera del peluquero porque este posee completa su casilla de turnos.")
+            bodyBuilder.appendln("Es muy probable que su turno demore mucho. Si quiere puede cancelar el turno en su pestaña de turnos.")
+        }
+
+        emailServiceAPI.sendEmail(turno.getClienteEmail(),
+                agregarNoReply("El turno esta en la cola de espera del peluquero \"${turno.getPeluqueroName()}\""),
+                bodyBuilder.toString()
+        )
     }
 
     fun enviarMailDeFinalizacionTurno(turno: Turno) {
-        emailServiceAPI.sendEmail(turno.cliente.emailOpcional,
-            "El turno ha finalizado, ya puede calificar el servicio",
-        "Hola, el turno ha sido marcado como finalizado por el peluquero. " +
-                "Puede calificar su turno en nuestra pagina en la sección \"Mis turnos\">\"Turnos Historicos\" "+
-                "para ofrecer un feedback del servicio contratado. " +
-                "En caso de haber existido algun tipo de problema, contactenos al email peluqueria-ya-soporte@gmail.com. "+
-                "Saludos y muchas gracias por usar nuestro servicio! :D")
+        val bodyBuilder = StringBuilder()
+        createBody(bodyBuilder) {
+            bodyBuilder.appendln("El turno ha sido marcado como finalizado por el peluquero.")
+            bodyBuilder.appendln("Puede calificar su turno en la opción de su perfil \"Mis turnos\", sección \"Historial\" para ofrecer un feedback del servicio contratado!")
+            bodyBuilder.appendln("En caso de haber existido algun tipo de problema, contactenos al email peluqueria-ya-soporte@gmail.com.")
+        }
+
+        emailServiceAPI.sendEmail(turno.getClienteEmail(),
+                agregarNoReply("El peluquero \"${turno.getPeluqueroName()}\" ha finalizado el turno, ya puede calificar el servicio"),
+                bodyBuilder.toString()
+        )
+    }
+
+    private fun agregarNoReply(str:String):String = "(No Reply) $str"
+
+    private fun createBody(bodyBuilder:StringBuilder, middleBodyMessage: () -> Unit) {
+        agregarSaludoEnBodyBuilder(bodyBuilder)
+        middleBodyMessage()
+        agregarAgradecimientoYSaludoEnBodyBuilder(bodyBuilder)
+    }
+
+    private fun agregarSaludoEnBodyBuilder(bodyBuilder: StringBuilder){
+        bodyBuilder.appendln("Hola, ¿Qué tal?")
+        bodyBuilder.appendln()
+    }
+
+    private fun agregarAgradecimientoYSaludoEnBodyBuilder(bodyBuilder: StringBuilder){
+        bodyBuilder.appendln()
+        bodyBuilder.appendln("Gracias por utilizar nuestro servicio!")
+        bodyBuilder.appendln("Saludos! :D")
+        bodyBuilder.appendln()
+        bodyBuilder.appendln("Peluqueria Ya")
     }
 }
